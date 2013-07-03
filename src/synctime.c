e575d54b92683846e51f125046ff9a87fac0df44
aragua <fabienlahoudere@gmail.com>
2013-07-03 23:42:33 +0200

    save

--- src/synctime.c
+++ src/synctime.c
@@ -9,7 +9,25 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-static struct timeval timestamp_buffer[10];
+typedef struct{
+    u_int64_t sec;
+    u_int64_t usec;
+} __attribute__((packed)) timestamp_t;
+
+#define TIMESTAMP_NUMBER 10
+static timestamp_t timestamp_buffer[TIMESTAMP_NUMBER];
+#define BUFFER_SIZE (sizeof(timestamp_t) * TIMESTAMP_NUMBER)
+
+
+timestamp_t timestamp()
+{
+    struct timeval ts;
+    timestamp_t ret;
+    gettimeofday( &ts, NULL );
+    ret.sec = (u_int64_t)ts.tv_sec;
+    ret.usec = (u_int64_t)ts.tv_usec;
+    return ret;
+}
 
 int start_sync_server( short port )
 {
@@ -52,14 +70,14 @@ int start_sync_server( short port )
     }
 
     /* Do treatment ... */
-    read( newsockd, timestamp_buffer, sizeof(struct timeval) * 10 );
-    gettimeofday( &timestamp_buffer[1], NULL);
-    gettimeofday( &timestamp_buffer[2], NULL);
-    write( newsockd, timestamp_buffer, sizeof(struct timeval) * 10 );
-    read( newsockd, timestamp_buffer, sizeof(struct timeval) * 10 );
-    gettimeofday( &timestamp_buffer[5], NULL);
-    gettimeofday( &timestamp_buffer[6], NULL);
-    write( newsockd, timestamp_buffer, sizeof(struct timeval) * 10 );
+    read( newsockd, timestamp_buffer, BUFFER_SIZE );
+    timestamp_buffer[1] = timestamp();
+    timestamp_buffer[2] = timestamp();
+    write( newsockd, timestamp_buffer, BUFFER_SIZE );
+    read( newsockd, timestamp_buffer, BUFFER_SIZE );
+    timestamp_buffer[5] = timestamp();
+    timestamp_buffer[6] = timestamp();
+    write( newsockd, timestamp_buffer, BUFFER_SIZE );
 
     close(newsockd);
     close(sockd);
@@ -91,22 +109,22 @@ int start_sync_client( char * server_addr, short port )
         exit(1);
     }
 
-    memset( timestamp_buffer, 0, sizeof(struct timeval) * 10 );
+    memset( timestamp_buffer, 0, BUFFER_SIZE );
 
-    gettimeofday( &timestamp_buffer[0], NULL);
-    write( sockd, timestamp_buffer, sizeof(struct timeval) * 10 );
-    read( sockd, timestamp_buffer, sizeof(struct timeval) * 10 );
-    gettimeofday( &timestamp_buffer[3], NULL);
-    gettimeofday( &timestamp_buffer[4], NULL);
-    write( sockd, timestamp_buffer, sizeof(struct timeval) * 10 );
-    read( sockd, timestamp_buffer, sizeof(struct timeval) * 10 );
-    gettimeofday( &timestamp_buffer[7], NULL);
+    timestamp_buffer[0] = timestamp();
+    write( sockd, timestamp_buffer, BUFFER_SIZE );
+    read( sockd, timestamp_buffer, BUFFER_SIZE );
+    timestamp_buffer[3] = timestamp();
+    timestamp_buffer[4] = timestamp();
+    write( sockd, timestamp_buffer, BUFFER_SIZE );
+    read( sockd, timestamp_buffer, BUFFER_SIZE );
+    timestamp_buffer[7] = timestamp();
 
     /* treat timestamp_buffer */
     printf("Synchronisation result:\n");
     for ( loop = 0 ; loop < 8 ; loop++ )
     {
-        printf("%d: %lu,%lu\n", loop, timestamp_buffer[loop].tv_sec, timestamp_buffer[loop].tv_usec );
+        printf("%d: %lu,%lu\n", loop, timestamp_buffer[loop].sec, timestamp_buffer[loop].usec );
     }
 
     close(sockd);
